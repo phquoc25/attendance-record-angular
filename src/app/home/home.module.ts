4d1e7da5bafc13882e86d268b59719ac601d1b66
Quoc PHAN <phquoc25@gmail.com>
2023-09-01 22:05:38 +0700

    implement display of player list

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import { PlayerListComponent } from '../player-list/player-list.component';
+import { PlayerComponent } from '../player/player.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    PlayerListComponent,
+    PlayerComponent
   ],
   imports: [
     CommonModule
